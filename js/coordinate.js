63be864b2746f5cc8a841b5d24521648c5deefb6
asher.dev <grit.developer.asher@gmail.com>
2021-03-19 13:05:22 +0900

    Improve performance about coordinates

--- js/coordinate.js
+++ js/coordinate.js
@@ -5,17 +5,18 @@ function init() {
   const vertical = document.querySelector('.vertical');
   const horizontal = document.querySelector('.horizontal');
   const coordinate = document.querySelector('.txt_coordinate');
+  const targetRect = target.getBoundingClientRect();
+  const rectHalfWidth = targetRect.width / 2;
+  const reactHalfHeight = targetRect.height / 2;
 
   document.addEventListener('mousemove', function (event) {
     const x = event.clientX;
     const y = event.clientY;
 
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    coordinate.style.left = `${x + 15}px`;
-    coordinate.style.top = `${y + 15}px`;
+    target.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
+    vertical.style.transform = `translate(${x}px, 0)`;
+    horizontal.style.transform = `translate(0, ${y}px)`;
+    coordinate.style.transform = `translate(${x - rectHalfWidth}px, ${y - reactHalfHeight}px)`;
     coordinate.innerText = `${x}px, ${y}px`;
   });
 }
